ad08d2ee238e8e91a0e93f1c561b6cde2237dc5f
Aditya Pathak <adityapathak@Adityas-MacBook-Pro.local>
2022-12-24 23:47:57 +0530

    completed contact me page

--- src/lib/components/Contact/ContactComp.svelte
+++ src/lib/components/Contact/ContactComp.svelte
@@ -4,6 +4,6 @@
     export let contact: Contact;
 </script>
 
-<div>
-    {contact.name}
+<div class="mx-auto flex">
+    <div class="my-2"><a href="{contact.url}">{@html contact.logo}</a></div>
 </div>
